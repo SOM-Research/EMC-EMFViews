e786104a3a15019344cdf812bfcba983c32c0379
fmdkdd <fmdkdd@0xc0de.fr>
2019-02-22 17:05:40 +0100

    Delegate getAllOfKind to View.getAllInstances
    14ms -> 3ms

--- plugins/org.eclipse.epsilon.emc.emfviews/src/org/eclipse/epsilon/emc/emfviews/EMFViewsModel.java
+++ plugins/org.eclipse.epsilon.emc.emfviews/src/org/eclipse/epsilon/emc/emfviews/EMFViewsModel.java
@@ -3,6 +3,7 @@ package org.eclipse.epsilon.emc.emfviews;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.atlanmod.emfviews.core.ViewResource;
 import org.eclipse.emf.ecore.EObject;
@@ -14,34 +15,38 @@ import fr.inria.atlanmod.neoemf.resource.PersistentResource;
 
 public class EMFViewsModel extends EmfModel {
 
-	@Override
-	public Collection<EObject> getAllOfKind(String kind) throws EolModelElementTypeNotFoundException {
-		System.out.println("EMF Views Driver: AllOfKind(" + kind + ")");
-		Instant start = Instant.now();
-		Collection<EObject> result = super.getAllOfKind(kind);
-		Instant end = Instant.now();
-		System.out.println("Done: " + Duration.between(start, end).toMillis() + "ms");
-		return result;
-	}
+  @Override
+  public Collection<EObject> getAllOfKind(String kind) throws EolModelElementTypeNotFoundException {
+    System.out.println("EMF Views Driver: AllOfKind(" + kind + ")");
+    Instant start = Instant.now();
 
-	/**
-	 * Unload the view.
-	 */
-	public void disposeModel() {
-		ViewResource r = (ViewResource) modelImpl;
-		/*
-		 * Quickfix: we need to manually unload the NeoEMF resource contributing to the
-		 * view, otherwise the database lock is not released and its not possible to
-		 * compute another EOL program on top of it.
-		 * 
-		 * @fmdkdd this should probably be moved in EMFViews core, we need to discuss this.
-		 */
-		for (Resource resource : r.getView().getContributingModels()) {
-			if (resource instanceof PersistentResource) {
-				((PersistentResource) resource).close();
-			}
-		}
-		super.disposeModel();
-	}
+    ViewResource r = (ViewResource) modelImpl;
+    Collection<EObject> result = r.getView().getAllInstances(kind).collect(Collectors.toList());
+
+    Instant end = Instant.now();
+    System.out.println("Done: " + Duration.between(start, end).toMillis() + "ms");
+    return result;
+  }
+
+  /**
+   * Unload the view.
+   */
+  @Override
+  public void disposeModel() {
+    ViewResource r = (ViewResource) modelImpl;
+    /*
+     * Quickfix: we need to manually unload the NeoEMF resource contributing to the
+     * view, otherwise the database lock is not released and its not possible to
+     * compute another EOL program on top of it.
+     *
+     * @fmdkdd this should probably be moved in EMFViews core, we need to discuss this.
+     */
+    for (Resource resource : r.getView().getContributingModels()) {
+      if (resource instanceof PersistentResource) {
+        ((PersistentResource) resource).close();
+      }
+    }
+    super.disposeModel();
+  }
 
 }
